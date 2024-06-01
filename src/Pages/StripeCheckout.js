b6362b3a721a825c23323a61f0847b3d11b59406
Tushar Gagal <tushargagal1209@gmail.com>
2024-06-01 21:18:30 +0530

    last commit

--- src/Pages/StripeCheckout.js
+++ src/Pages/StripeCheckout.js
@@ -19,7 +19,7 @@ export default function StripeCheckout() {
   const currentOrder = useSelector(selectCurrentOrder);
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:8080/create-payment-intent", {
+    fetch("/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
